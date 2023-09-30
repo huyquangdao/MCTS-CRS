2d6531df91501b79d0f590ec3c01a6e0bafd3069
huyquangdao <huydao98.uet@gmail.com>
2023-09-30 10:54:02 +0800

    fix sh file

--- scripts/infer_policy.sh
+++ scripts/infer_policy.sh
@@ -6,7 +6,6 @@ CUDA_VISIBLE_DEVICES=5 accelerate launch --gpu_ids 5 infer_policy.py \
     --test_data_path data/DuRecDial/data/en_test.txt \
     --tokenizer roberta-base \
     --plm_model roberta-base \
-    --num_train_epochs 5 \
     --hidden_size 128 \
     --lm_size 768 \
     --output_dir ./policy_model/ \
