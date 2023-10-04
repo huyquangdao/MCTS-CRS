1837fc04f95a8c97972e7dc6a5768ddf3f46fb8a
huyquangdao <huydao98.uet@gmail.com>
2023-10-04 14:11:38 +0800

    modify mcts sh file

--- scripts/run_mcts_dialogue_planning.sh
+++ scripts/run_mcts_dialogue_planning.sh
@@ -4,13 +4,17 @@ CUDA_VISIBLE_DEVICES=5 accelerate launch --gpu_ids 5 examples/uct_dialogue_plann
     --train_data_path data/DuRecDial/data/en_train.txt \
     --dev_data_path data/DuRecDial/data/en_dev.txt \
     --test_data_path data/DuRecDial/data/en_test.txt \
-    --tokenizer roberta-base \
-    --plm_model roberta-base \
+    --policy_tokenizer roberta-base \
+    --policy_plm_model roberta-base \
+    --generation_tokenizer facebook/bart-base \
+    --generation_plm_model facebook/bart-base \
     --rollouts 20 \
     --alg p_uct \
     --hidden_size 128 \
     --lm_size 768 \
     --horizon 5 \
     --max_sequence_length 512 \
-    --model_path ./policy_model/ \
+    --max_gen_length 50 \
+    --policy_model_path ./policy_model/ \
+    --generation_model_path ./generation_model/
     --seed 22
