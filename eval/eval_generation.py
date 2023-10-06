e4fd36219c0a4ddfaa0f989cd16225766f5acb39
huyquangdao <huydao98.uet@gmail.com>
2023-10-06 11:56:36 +0800

    add f1 metric

--- eval/eval_generation.py
+++ eval/eval_generation.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 import json
 from nltk import ngrams
@@ -15,13 +16,20 @@ class GenerationEvaluator:
             self.log_cnt = 0
 
     def evaluate(self, preds, labels, log=False):
+        """
+        methods that compute a set of evaluation metrics given the generated text strings and groundtruth labels
+        @param preds: a list of generated text strings
+        @param labels: a list of groundtruth labels
+        @param log: log to file
+        @return: None
+        """
         decoded_preds = self.tokenizer.batch_decode(preds, skip_special_tokens=False)
-        decoded_preds = [decoded_pred.replace('<pad>', '').replace('<|endoftext|>', '') for decoded_pred in
+        decoded_preds = [decoded_pred.replace('</s>', '').replace('<s>', '') for decoded_pred in
                          decoded_preds]
         decoded_preds = [pred.strip() for pred in decoded_preds]
 
         decoded_labels = self.tokenizer.batch_decode(labels, skip_special_tokens=False)
-        decoded_labels = [decoded_label.replace('<pad>', '').replace('<|endoftext|>', '') for decoded_label in
+        decoded_labels = [decoded_label.replace('</s>', '').replace('<s>', '') for decoded_label in
                           decoded_labels]
         decoded_labels = [label.strip() for label in decoded_labels]
 
@@ -34,9 +42,15 @@ class GenerationEvaluator:
 
         self.collect_ngram(decoded_preds)
         self.compute_bleu(decoded_preds, decoded_labels)
+        self.compute_word_f1(decoded_preds, decoded_labels)
         self.sent_cnt += len([pred for pred in decoded_preds if len(pred) > 0])
 
     def collect_ngram(self, strs):
+        """
+        method that compute n-grams (n=1,4) of a set of strings.
+        @param strs: a list of text strings
+        @return: None
+        """
         for str in strs:
             str = str.split()
             for k in range(1, 5):
@@ -45,6 +59,12 @@ class GenerationEvaluator:
                     self.metric[dist_k].add(token)
 
     def compute_bleu(self, preds, labels):
+        """
+        method that compute bleu score between generated texts and labels
+        @param preds: a list of generated text strings
+        @param labels: a list of groundtruth labels
+        @return: None
+        """
         for pred, label in zip(preds, labels):
             pred, label = pred.split(), [label.split()]
             for k in range(4):
@@ -52,7 +72,31 @@ class GenerationEvaluator:
                 weights[k] = 1
                 self.metric[f'bleu@{k + 1}'] += sentence_bleu(label, pred, weights)
 
+    def compute_word_f1(self, preds, labels):
+        """
+        method that compute word-level f1 score between generated text string and labels
+        @param preds: a list of generated text strings
+        @param labels: a list of groundtruth labels
+        @return: none
+        """
+        golden_char_total = 0.0
+        pred_char_total = 0.0
+        hit_char_total = 0.0
+        for response, golden_response in zip(preds, labels):
+            common = Counter(response) & Counter(golden_response)
+            hit_char_total += sum(common.values())
+            golden_char_total += len(golden_response)
+            pred_char_total += len(response)
+        p = hit_char_total / pred_char_total if pred_char_total > 0 else 0
+        r = hit_char_total / golden_char_total if golden_char_total > 0 else 0
+        f1 = 2 * p * r / (p + r) if p + r > 0 else 0
+        self.metric["f1"] = f1
+
     def report(self):
+        """
+        methods that compute the final values of metrics
+        @return: None
+        """
         report = {}
         for k, v in self.metric.items():
             if self.sent_cnt == 0:
@@ -65,11 +109,16 @@ class GenerationEvaluator:
         return report
 
     def reset_metric(self):
+        """
+        method that inits default values for the metrics
+        @return: None
+        """
         self.metric = {
             'bleu@1': 0,
             'bleu@2': 0,
             'bleu@3': 0,
             'bleu@4': 0,
+            "f1": 0,
             'dist@1': set(),
             'dist@2': set(),
             'dist@3': set(),
