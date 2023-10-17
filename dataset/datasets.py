6f360ce57898338911d061fa7429e014fbe9efe3
huyquangdao <huydao98.uet@gmail.com>
2023-10-17 19:49:53 +0800

    fix unimind torchdataset

--- dataset/datasets.py
+++ dataset/datasets.py
@@ -5,36 +5,6 @@ from dataset.base import BaseTorchDataset
 
 class UnimindTorchDataset(BaseTorchDataset):
 
-    def __init__(self, tokenizer, instances, goal2id=None, max_sequence_length=512, padding='max_length',
-                 pad_to_multiple_of=True, device=None, convert_example_to_feature=None, max_target_length=50,
-                 is_test=False, is_gen=False):
-        """
-        constructor for the BaseTorchDataset Class
-        @param tokenizer: an huggingface tokenizer
-        @param instances: a list of instances
-        @param goal2id: a dictionary which maps goal to index.
-        @param max_sequence_length: the maximum length of the input sequence.
-        @param padding: type of padding
-        @param pad_to_multiple_of: pad to multiple instances
-        @param device: device to allocate the data, eg: cpu or gpu
-        @param convert_example_to_feature: a function that convert raw instances to
-        corresponding inputs and labels for the model.
-        @param max_target_length the maximum number of the target sequence (response generation only)
-        @param is_test True if inference step False if training step
-        @param is_gen True if response generation else False
-        """
-        super(BaseTorchDataset, self).__init__()
-        self.max_sequence_length = max_sequence_length
-        self.tokenizer = tokenizer
-        self.goal2id = goal2id
-        self.pad_to_multiple_of = pad_to_multiple_of
-        self.padding = padding
-        self.device = device
-        self.max_target_length = max_target_length
-        self.is_test = is_test
-        self.is_gen = is_gen
-        self.instances = self.__preprocess_data(instances, convert_example_to_feature)
-
     def __preprocess_data(self, instances, convert_example_to_feature):
 
         assert isinstance(convert_example_to_feature, dict) is True
