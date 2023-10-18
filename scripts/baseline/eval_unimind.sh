0ac4066a0ddf99b9a56fbf0fdcfc1e5059a070ce
huyquangdao <huydao98.uet@gmail.com>
2023-10-18 14:01:30 +0800

    modify unimind eval sh file

--- scripts/baseline/eval_unimind.sh
+++ scripts/baseline/eval_unimind.sh
@@ -27,3 +27,18 @@ CUDA_VISIBLE_DEVICES=5 accelerate launch --gpu_ids 5 baselines/unimind/infer_top
     --output_dir ./unimind/ \
     --seed 12
 
+#infer response
+CUDA_VISIBLE_DEVICES=5 accelerate launch --gpu_ids 5 baselines/unimind/infer_response_unimind.py \
+    --train_data_path data/DuRecDial/data/en_train.txt \
+    --dev_data_path data/DuRecDial/data/en_dev.txt \
+    --test_data_path data/DuRecDial/data/en_test.txt \
+    --tokenizer facebook/bart-base \
+    --plm_model facebook/bart-base \
+    --per_device_eval_batch_size 32 \
+    --max_sequence_length 512 \
+    --max_target_length 80 \
+    --goal_outpath ./unimind/ \
+    --topic_outpath ./unimind/ \
+    --output_dir ./unimind/ \
+    --seed 12
+
