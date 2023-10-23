f310ef5ebd79e226050d391dac419b276dc5f505
huyquangdao <huydao98.uet@gmail.com>
2023-10-23 15:04:39 +0800

    initial implementation for retrieval

--- retrieval/retrieval.py
+++ retrieval/retrieval.py
@@ -5,23 +5,27 @@ import numpy as np
 
 class Memory:
 
-    def __init__(self, embedding_model, raw_memory, d_model):
+    def __init__(self, embedding_model, raw_memory, d_model=384, ngpu=1):
         self.embedding_model = embedding_model
         self.raw_memory = raw_memory
         self.d_model = d_model
-
+        self.ngpu = ngpu
         self.index = self.build_index()
 
     def build_index(self):
         sentence_embeddings = self.embedding_model.encode(self.raw_memory)
+        resources = [faiss.StandardGpuResources() for i in range(self.ngpu)]
         assert sentence_embeddings.shape[0] == len(self.raw_memory)
         assert sentence_embeddings.shape[1] == self.d_model
-        sentence_embeddings = sentence_embeddings.detach().numpy()
-        assert isinstance(sentence_embeddings, np.array)
         index = faiss.IndexFlatL2(self.d_model)  # build the index
         index.add(sentence_embeddings)  # add vectors to the index
-        return index
+        index_gpu = faiss.index_cpu_to_gpu_multiple_py(resources, index)
+        return index_gpu
 
     def search(self, queries, k=10):
         D, I = self.index.search(queries, k)
         return D, I
+
+    def update(self, new_memories):
+        self.raw_memory.extend(new_memories)
+        self.index = self.build_index()
--- retrieval/utils.py
+++ retrieval/utils.py
@@ -1,13 +1,4 @@
 import os
-import faiss
-
-
-def build_indexes(embed_model, memory):
-
-    sentence_embeds = embed_model.encode(memory)
-
-def retrieve_candidates(query, indexes, k=10):
-    pass
 
 
 def build_mcts_memory(train_convs):
