6994ad78b38c4e69d27624032bcb47e5823ef68b
huyquangdao <huydao98.uet@gmail.com>
2023-10-23 16:34:20 +0800

    fix retrieval

--- retrieval/retrieval.py
+++ retrieval/retrieval.py
@@ -23,12 +23,14 @@ class Memory:
         assert sentence_embeddings.shape[0] == len(self.raw_memory)
         assert sentence_embeddings.shape[1] == self.d_model
         index = faiss.IndexFlatIP(self.d_model)  # build the index
-        index.add(faiss.normalize_l2(sentence_embeddings))  # add vectors to the index
+        # add vectors to the index
+        faiss.normalize_L2(sentence_embeddings)
+        index.train(sentence_embeddings)
         index_gpu = faiss.index_cpu_to_gpu(device, 0, index)
         return index_gpu
 
     def search(self, queries, k=10):
-        D, I = self.index.search(faiss.normalize_l2(queries), k)
+        D, I = self.index.search(faiss.normalize_L2(queries), k)
         return D, I
 
     def update(self, new_memories):
