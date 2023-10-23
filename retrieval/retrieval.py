4ca9f25bcebceda687b7588294bef3ca9a60477b
huyquangdao <huydao98.uet@gmail.com>
2023-10-23 16:28:19 +0800

    fix retrieval

--- retrieval/retrieval.py
+++ retrieval/retrieval.py
@@ -5,25 +5,30 @@ import numpy as np
 
 class Memory:
 
-    def __init__(self, embedding_model, raw_memory, d_model=384, ngpu=1):
+    def __init__(self, embedding_model, raw_memory, d_model=384):
+        """
+        constructor for class me memory
+        @param embedding_model: the sentence embedding model
+        @param raw_memory: a set of raw dialogue contexts
+        @param d_model: the dimension of vector indexes
+        """
         self.embedding_model = embedding_model
         self.raw_memory = raw_memory
         self.d_model = d_model
-        self.ngpu = ngpu
         self.index = self.build_index()
 
     def build_index(self):
         sentence_embeddings = self.embedding_model.encode(self.raw_memory)
-        resources = [faiss.StandardGpuResources() for i in range(self.ngpu)]
+        device = faiss.StandardGpuResources()
         assert sentence_embeddings.shape[0] == len(self.raw_memory)
         assert sentence_embeddings.shape[1] == self.d_model
-        index = faiss.IndexFlatL2(self.d_model)  # build the index
-        index.add(sentence_embeddings)  # add vectors to the index
-        index_gpu = faiss.index_cpu_to_gpu_multiple_py(resources, index)
+        index = faiss.IndexFlatIP(self.d_model)  # build the index
+        index.add(faiss.normalize_l2(sentence_embeddings))  # add vectors to the index
+        index_gpu = faiss.index_cpu_to_gpu(device, 0, index)
         return index_gpu
 
     def search(self, queries, k=10):
-        D, I = self.index.search(queries, k)
+        D, I = self.index.search(faiss.normalize_l2(queries), k)
         return D, I
 
     def update(self, new_memories):
--- retrieval/utils.py
+++ retrieval/utils.py
@@ -1,5 +1,14 @@
 import os
 
 
-def build_mcts_memory(train_convs):
-    pass
+def construct_mcts_memory(train_instances):
+    """
+    function that build a memory from using the training dataset.
+    @param train_instances: a list of training instances.
+    @return: a list of dialogue contexts
+    """
+    raw_memory = []
+    for instance in train_instances:
+        context = instance['dialogue_context']
+        raw_memory.append(context)
+    return raw_memory
