a80003a57604df4e9c148edc99edeff9d1c77237
huyquangdao <huydao98.uet@gmail.com>
2023-12-05 16:39:32 +0800

    implememnt 1-shot llm based target driven assessment

--- dyna_gym/envs/utils.py
+++ dyna_gym/envs/utils.py
@@ -771,11 +771,11 @@ def get_llm_based_assessment(target_topic, simulated_conversation, demonstration
 
     # assessment instruction
     system_instruction_3 = '''Based on the given conversation, you need to infer the attitude of the user towards the 
-    target item : {}. You need to predict if the user is happy or willing to accept the target item. 
+    target item : {}. You need to predict if the user is happy or willing to accept the target item {}. 
     If the user is convinced and happy, you need to generate the word: {}
     If the user is confused or unhappy or if the target item :{} is not in the given conversation, 
     you need to generate the word: {}
-    '''.format(target_topic, accept_string, target_topic, reject_string)
+    '''.format(target_topic, target_topic, accept_string, target_topic, reject_string)
 
     # the new generate response.
     messages.append(
